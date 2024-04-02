edeb2c51dca3333fcb6a6edde87ea8343e371fc4
Andy Magdalinos <144169482+AndyMagD@users.noreply.github.com>
2024-04-02 14:37:07 +0300

    Add files via upload

---
+++ Day 63 BookManager/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book-collection.db'
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    all_books = Book.query.all()
+    return render_template("index.html", all_books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=float(request.form["rating"])
+        )
+        db.session.add(new_book)
+        db.session.commit()
+
+        return redirect(url_for("home"))
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
